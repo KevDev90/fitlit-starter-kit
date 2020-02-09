f26ecd0f70ca41ef41d0ecc8d63128f95dd05bf0
kevin johnson <kevinj902302@gmail.com>
2020-02-08 17:55:13 -0700

    Built out the activityRepository class methods

--- src/activityRepository.js
+++ src/activityRepository.js
@@ -1,16 +1,22 @@
 class ActivityRepository {
   constructor(activityData) {
     this.activityData = activityData;
+    this.userInfo;
   }
 
-  getUserById() {
-
+  getUserById(id) {
+    this.userInfo = this.activityData.filter(user => user.userID === id);
+    return this.userInfo;
   }
 
   getAllAvgActivityByDate(activity, date) {
-
+    let dateSpecificDatas = this.activityData.filter(loggedActivity =>
+      loggedActivity.date === date)
+    let totaledUpActivity = dateSpecificDatas.reduce((acc, loggedActivity) =>
+      acc += loggedActivity[activity], 0);
+    return Math.round(totaledUpActivity / dateSpecificDatas.length);
   }
-  
+
 
 }
 
